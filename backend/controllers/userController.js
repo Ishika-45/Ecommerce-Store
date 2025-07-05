b94342523cbeac8b966915e9fe7a667e9b69cdae
Ishika-45 <bansalishika58@gmail.com>
2025-07-05 11:26:55 +0530

    connecting backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,31 @@
-import asyncHandler from "../middlewares/asyncHandler";
-import User from "../models/userModel";
-const createUser = asyncHandler(async (req, res) => {
-    res.send("hello");
-});
+import asyncHandler from "../middlewares/asyncHandler.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
-export default createUser;
+export const createUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        throw new Error("Please fill all the details.");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) res.status(400).send("User already exists");
+
+    const newUser = new User({ username, email, password })
+
+    try {
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin
+        })
+    }
+    catch (error) {
+        res.status(400)
+        throw new Error("Invalid User data")
+    }
+}); 
